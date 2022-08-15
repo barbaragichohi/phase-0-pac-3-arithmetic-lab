d3e3852662e0d9b3920212758dc0f4c0eba6d48d
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-15 20:45:12 +0300

    Completed Assigment

---
+++ index.js
@@ -0,0 +1,49 @@
+function add () {}
+function subtract () {}
+function multiply () {}
+function divide () {}
+
+
+const a = 5;
+const b = 9;
+
+
+
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function increment (a) {
+    return a + 1;
+}
+function decrement (a) {
+    return a - 1;
+}
+
+function makeInt (a) {
+    return 'n', a;
+}
+
+function makeInt (n) {
+    return n;
+}
+
+
+function preserveDecimal (n= '2.222') {
+    return n = Number.parseFloat (n);
+}
+
+
